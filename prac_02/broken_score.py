341ef60c11ea53ce82acdd7358957da83ab6d7a2
Arpisitt Numsri <arpisitt.numsri@my.jcu.edu.au>
2023-06-11 11:14:48 +1000

    Refactor score program to use functions

--- prac_02/broken_score.py
+++ prac_02/broken_score.py
@@ -2,16 +2,27 @@
 CP1404/CP5632 - Practical
 Broken program to determine score status
 """
-
-score = float(input("Enter score: "))
-if 0 > score > 100:
-    print("Invalid score")
-elif score >= 50:
-    print("Passable")
-elif score >= 90:
-    print("Excellent")
-else:
-    print("Bad")
+import random
 
 
+def main():
+    """Get score and display its rating."""
 
+    score = float(input("Enter score: "))
+    print(get_rating(score))
+    print(random.randint(0, 100))
+
+
+def get_rating(score):
+    """Determine rating of the score."""
+    if 0 > score > 100:
+        return "Invalid score"
+    elif score >= 50:
+        return "Passable"
+    elif score >= 90:
+        return "Excellent"
+    else:
+        return "Bad"
+
+
+main()
