dad45cd56d196a0f8aac29513bfa05d3f2171d6d
Arpisitt Numsri <arpisitt.numsri@my.jcu.edu.au>
2023-07-23 14:19:55 +1000

    Add error checking for all functions

--- prac_07/project_management.py
+++ prac_07/project_management.py
@@ -52,40 +52,49 @@ def update_projects(projects):
     """Update existing functions completion rate and priority"""
     for i, project in enumerate(projects):
         print(i, project)
-    choice = int(input("Project choice: "))
-    new_percentage = input("New Percentage: ")
-    new_priority = input("New Priority: ")
-    if new_percentage != "":
-        projects[choice].completion_percentage = int(new_percentage)
-    if new_priority != "":
-        projects[choice].priority = int(new_priority)
+    try:
+        choice = int(input("Project choice: "))
+        new_percentage = input("New Percentage: ")
+        new_priority = input("New Priority: ")
+
+        if new_percentage != "":
+            projects[choice].completion_percentage = int(new_percentage)
+        if new_priority != "":
+            projects[choice].priority = int(new_priority)
+    except ValueError:
+        print("Invalid input")
 
 
 def add_project(projects):
     """Add a new project to the list"""
-    print("Lets add a new project")
+    print("Let's add a new project")
     name = input("Name: ")
     start_date = input("Start date (dd/mm/yyyy): ")
-    priority = int(input("Priority: "))
-    cost_estimate = float(input("Cost estimate: "))
-    completion_rate = int(input("Percent complete: "))
-    project = Project(name, start_date, cost_estimate, completion_rate, priority)
-    projects.append(project)
+    cost_estimate = input("Cost estimate: ")
+    completion_rate = input("Percent complete: ")
+    try:
+        priority = int(input("Priority: "))
+        cost_estimate = float(cost_estimate)
+        completion_rate = int(completion_rate)
+        project = Project(name, start_date, cost_estimate, completion_rate, priority)
+        projects.append(project)
+    except ValueError:
+        print("Invalid input.")
 
 
 def filter_by_date(projects, date_str):
     """Filter projects by given date"""
     print(f"Enter Date (dd/mm/yyyy): {date_str}")
-    filtered_projects = []
-    for project in projects:
-        if project.start_date > date_str:
-            filtered_projects.append(project)
-    if filtered_projects:
-        for project in filtered_projects:
-            print(f"{project.name}, start: {project.start_date}, priority {project.priority}, "
-                  f"estimate: ${project.cost_estimate:.2f}, completion: {project.completion_rate}%")
-    else:
-        print("No projects found!")
+    try:
+        filtered_projects = [project for project in projects if project.start_date > date_str]
+        if filtered_projects:
+            for project in filtered_projects:
+                print(f"{project.name}, start: {project.start_date}, priority {project.priority}, "
+                      f"estimate: ${project.cost_estimate:.2f}, completion: {project.completion_rate}%")
+        else:
+            print("No projects found!")
+    except ValueError:
+        print("Invalid date format.")
 
 
 def display_projects(projects):
@@ -104,16 +113,19 @@ def display_projects(projects):
 def load_projects(filename):
     """Load project from given file"""
     projects = []
-    with open(filename, 'r') as file:
-        file.readline()
-        for line in file:
-            name, start_date, priority, cost_estimate, completion_rate = line.strip().split('\t')
-            cost_estimate = float(cost_estimate)
-            completion_rate = int(completion_rate)
-            priority = int(priority)
-            project = Project(name, start_date, cost_estimate, completion_rate, priority)
-            projects.append(project)
-    print(f"Loaded {filename}")
+    try:
+        with open(filename, 'r') as file:
+            file.readline()
+            for line in file:
+                name, start_date, priority, cost_estimate, completion_rate = line.strip().split('\t')
+                cost_estimate = float(cost_estimate)
+                completion_rate = int(completion_rate)
+                priority = int(priority)
+                project = Project(name, start_date, cost_estimate, completion_rate, priority)
+                projects.append(project)
+        print(f"Loaded {filename}")
+    except FileNotFoundError:
+        print(f"File not found")
     return projects
 
 
