8cf93fb49dd53290dd95c55a94a73a43d1f60de3
Arpisitt Numsri <arpisitt.numsri@my.jcu.edu.au>
2023-07-30 09:50:02 +1000

    Add error handling to app

--- prac_08/convert_miles_km.py
+++ prac_08/convert_miles_km.py
@@ -11,25 +11,33 @@ from kivy.lang import Builder
 MILES_TO_KM = 1.60934
 
 
-class MilesConverter(App):
+class MilesApp(App):
     def build(self):
-
+        """Constructor for Miles converter program"""
         self.title = "Convert Miles to Kilometres"
         self.root = Builder.load_file('convert_miles_km.kv')
         return self.root
 
     def handle_calculate(self):
+        """Validate input and convert to km"""
         value = self.get_validated_miles()
         result = value * MILES_TO_KM
         self.root.ids.output_label.text = str(result)
 
     def handle_increment(self, change):
+
         value = self.get_validated_miles() + change
         self.root.ids.input_text.text = str(value)
         self.handle_calculate()
 
     def get_validated_miles(self):
+        """Checks if user input is a float, otherwise return error"""
+        try:
+            value = float(self.root.ids.input_miles.text)
+            return value
+        except ValueError:
+            return 0.0
 
 
 
-MilesConverter().run()
+MilesApp().run()
