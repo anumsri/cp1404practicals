485eaa08b09d09b1956304abd59b985576f303f6
Arpisitt Numsri <arpisitt.numsri@my.jcu.edu.au>
2023-08-03 10:11:02 +1000

    Adjust taxi to use class variable for price per km

--- prac_09/taxi_test.py
+++ prac_09/taxi_test.py
@@ -8,7 +8,7 @@ from taxi import Taxi
 
 def main():
     """Testing for Taxi Class"""
-    my_taxi = Taxi("Prius 1", 100, 1.23)
+    my_taxi = Taxi("Prius 1", 100)
     my_taxi.drive(40)
     print(my_taxi)
     my_taxi.start_fare()
