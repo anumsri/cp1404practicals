1358ea885c900a7eb257aaf31b7d9993560fa7f8
Arpisitt Numsri <arpisitt.numsri@my.jcu.edu.au>
2023-08-03 10:03:46 +1000

    Add testing for taxi class

--- prac_09/taxi_test.py
+++ prac_09/taxi_test.py
@@ -1,5 +1,13 @@
+"""
+CP1404 Arpisitt Numsri
+Testing for taxi class
+"""
+
 from taxi import Taxi
 
 my_taxi = Taxi("Prius 1", 100, 1.23)
 my_taxi.drive(40)
+print(my_taxi)
+my_taxi.start_fare()
+my_taxi.drive(100)
 print(my_taxi)
