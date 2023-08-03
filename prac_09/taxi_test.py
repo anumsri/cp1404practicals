c31ca6c92c33a5e567bbdb9d6c430d2b62c0bc4e
Arpisitt Numsri <arpisitt.numsri@my.jcu.edu.au>
2023-08-03 10:08:43 +1000

    Fix Taxi Test function

--- prac_09/taxi_test.py
+++ prac_09/taxi_test.py
@@ -5,9 +5,15 @@ Testing for taxi class
 
 from taxi import Taxi
 
-my_taxi = Taxi("Prius 1", 100, 1.23)
-my_taxi.drive(40)
-print(my_taxi)
-my_taxi.start_fare()
-my_taxi.drive(100)
-print(my_taxi)
+
+def main():
+    """Testing for Taxi Class"""
+    my_taxi = Taxi("Prius 1", 100, 1.23)
+    my_taxi.drive(40)
+    print(my_taxi)
+    my_taxi.start_fare()
+    my_taxi.drive(100)
+    print(my_taxi)
+
+
+main()
