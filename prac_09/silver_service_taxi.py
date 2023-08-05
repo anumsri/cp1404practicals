aa085e2e23bbf121bb3eaa4d22b5207a5229afed
Arpisitt Numsri <arpisitt.numsri@my.jcu.edu.au>
2023-08-05 17:07:19 +1000

    Initialise SilverServiceTaxi class

--- prac_09/silver_service_taxi.py
+++ prac_09/silver_service_taxi.py
@@ -6,5 +6,12 @@ from prac_09.taxi import Taxi
 
 
 class SilverServiceTaxi(Taxi):
-    def __init__(self):
 
+    def __init__(self, name, fuel, fanciness):
+        """Initialise a Taxi instance, based on parent class Car."""
+        super().__init__(name, fuel)
+        self.fanciness = fanciness
+        self.price_per_km *= fanciness
+
+    def __str__(self):
+        return f"{super().__str__()}"
--- prac_09/silver_service_taxi_test.py
+++ prac_09/silver_service_taxi_test.py
@@ -2,3 +2,11 @@
 CP1404/CP5632 Practical
 Silver service taxi testing
 """
+from prac_09.silver_service_taxi import SilverServiceTaxi
+
+
+def main():
+    my_taxi = SilverServiceTaxi("Silver Taxi", 60, 4)
+    my_taxi.drive(20)
+    print(my_taxi)
+    # print(my_taxi.get_fare())
