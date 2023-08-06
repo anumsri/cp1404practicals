3ab01ad86762782af4ca50ff7cbb4b6aedda7591
Arpisitt Numsri <arpisitt.numsri@my.jcu.edu.au>
2023-08-06 15:21:31 +1000

    Create function to display numbered taxi list

--- prac_09/taxi_simulator.py
+++ prac_09/taxi_simulator.py
@@ -10,6 +10,7 @@ MENU = "q)uit, c)hoose taxi, d)rive"
 
 
 def main():
+    """Taxi Simulator main program"""
     taxi_list = [Taxi("Prius", 100), SilverServiceTaxi("Limo", 100, 2),
                  SilverServiceTaxi("Hummer", 200, 4)]
     print("Let's drive!")
@@ -17,12 +18,21 @@ def main():
     choice = input(">>> ").lower()
     while choice != "q":
         if choice == "c":
-            pass
+            print("Taxis Available")
+            display_taxis(taxi_list)
+
         elif choice == "d":
             pass
         else:
             print("Invalid choice")
         print(MENU)
         choice = input(">>> ").lower()
+
+
+def display_taxis(taxi_list):
+    """Display taxi list nicely"""
+    for i, taxi in enumerate(taxi_list):
+        print(f"{i} - {taxi}")
+
 
 main()
