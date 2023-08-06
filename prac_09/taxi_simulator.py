515015f6cd48a61ced8aeb1dc43d8f731e616cc2
Arpisitt Numsri <arpisitt.numsri@my.jcu.edu.au>
2023-08-06 15:16:23 +1000

    Initial Commit

---
+++ prac_09/taxi_simulator.py
@@ -0,0 +1,9 @@
+"""
+Taxi Simulator for CP1404
+Arpisitt Numsri
+"""
+from prac_06.car import Car
+from prac_09.taxi import Taxi
+from prac_09.silver_service_taxi import SilverServiceTaxi
+
+MENU = "q)uit, c)hoose taxi, d)rive"
