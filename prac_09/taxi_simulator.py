96f612df5807b062f68a74ccdd9a283bcf430b55
Arpisitt Numsri <arpisitt.numsri@my.jcu.edu.au>
2023-08-06 15:19:03 +1000

    Add menu layout for program

--- prac_09/taxi_simulator.py
+++ prac_09/taxi_simulator.py
@@ -7,3 +7,22 @@ from prac_09.taxi import Taxi
 from prac_09.silver_service_taxi import SilverServiceTaxi
 
 MENU = "q)uit, c)hoose taxi, d)rive"
+
+
+def main():
+    taxi_list = [Taxi("Prius", 100), SilverServiceTaxi("Limo", 100, 2),
+                 SilverServiceTaxi("Hummer", 200, 4)]
+    print("Let's drive!")
+    print(MENU)
+    choice = input(">>> ").lower()
+    while choice != "q":
+        if choice == "c":
+            pass
+        elif choice == "d":
+            pass
+        else:
+            print("Invalid choice")
+        print(MENU)
+        choice = input(">>> ").lower()
+
+main()
