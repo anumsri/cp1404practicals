d2d1222a8203e97aa813046fa047de033be6a502
Arpisitt Numsri <arpisitt.numsri@my.jcu.edu.au>
2023-08-06 15:08:41 +1000

    Fix str representation to correctly produce output

--- prac_09/band.py
+++ prac_09/band.py
@@ -6,7 +6,13 @@ class Band:
 
     def __init__(self, name=""):
         self.name = name
-        self.musicians
+        self.musicians = []
 
     def __str__(self):
-        return f"{self.name} ({self.instruments})"
+        musician_strings = []
+        for musician in self.musicians:
+            musician_strings.append(str(musician))
+        return f"{self.name} ({', '.join(musician_strings)})"
+
+    def add(self, musician):
+        self.musicians.append(musician)
