c1f239fee3146ddf8ca31f4b5f7c1a8f3a3a30fa
Arpisitt Numsri <arpisitt.numsri@my.jcu.edu.au>
2023-08-13 15:39:38 +1000

    Complete main function of wiki program

--- prac_10/wiki.py
+++ prac_10/wiki.py
@@ -1,1 +1,16 @@
 """Wikipedia Program CP1404"""
+
+import wikipedia
+
+
+def main():
+    while True:
+        user_input = input("Enter page title or search phrase: ")
+        if not user_input:
+            break
+        page = wikipedia.page(user_input)
+        print("Summary:")
+        print(page.summary)
+
+
+main()
