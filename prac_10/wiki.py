67dae3f3e95c81035a13490962fe4287f1cf07f5
Arpisitt Numsri <arpisitt.numsri@my.jcu.edu.au>
2023-08-13 15:42:08 +1000

    Add exception handling

--- prac_10/wiki.py
+++ prac_10/wiki.py
@@ -8,9 +8,16 @@ def main():
         user_input = input("Enter page title or search phrase: ")
         if not user_input:
             break
-        page = wikipedia.page(user_input)
-        print("Summary:")
-        print(page.summary)
+
+        try:
+            page = wikipedia.page(user_input)
+            if page.exists():
+                print("Summary:")
+                print(page.summary)
+            else:
+                print("Not found")
+        except wikipedia.exceptions.DisambiguationError as e:
+            print(e.options)
 
 
 main()
